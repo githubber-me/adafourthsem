b67832f6d95519c488efe54fb7b8e5e9cf20b92a
Anthahkarana <100427124+githubber-me@users.noreply.github.com>
2024-08-04 19:21:02 +0530

    N Queens

--- nqueens.c
+++ nqueens.c
@@ -17,7 +17,6 @@ int main()
     return 0;
 }
 
-// Function for printing the solution
 void print(int n) 
 {
     int i, j;
@@ -29,34 +28,30 @@ void print(int n)
     for (i = 1; i <= n; ++i) 
     {
         printf("\n\n%d", i);
-        for (j = 1; j <= n; ++j) // For nxn board
+        for (j = 1; j <= n; ++j)
         {
             if (board[i] == j)
-                printf("\tQ"); // Queen at i,j position
+                printf("\tQ"); 
             else
-                printf("\t-"); // Empty slot
+                printf("\t-"); 
         }
     }
 }
 
-// Function to check conflicts
-// If no conflict for desired position returns 1, otherwise returns 0
 int place(int row, int column) 
 {
     int i;
     for (i = 1; i <= row - 1; ++i) 
     {
-        // Checking column and diagonal conflicts
         if (board[i] == column)
             return 0;
         else if (abs(board[i] - column) == abs(i - row))
             return 0;
     }
 
-    return 1; // No conflicts
+    return 1; 
 }
 
-// Function to check for proper positioning of queen
 void queen(int row, int n) 
 {
     int column;
@@ -64,10 +59,10 @@ void queen(int row, int n)
     {
         if (place(row, column)) 
         {
-            board[row] = column; // No conflicts so place queen
-            if (row == n) // Dead end
-                print(n); // Printing the board configuration
-            else // Try queen with next position
+            board[row] = column;
+            if (row == n) 
+                print(n); 
+            else
                 queen(row + 1, n);
         }
     }
